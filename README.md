2e0081869e04c24924db50b4d3af27fdc72aa625
Lukas <lukas.kubala@gmx.de>
2025-08-13 16:24:17 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spearhead Ability Tracker
---
+++ src/test/java/com/lukulabakas/spearheadAbilityTracker/SpearheadAbilityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukulabakas.spearheadAbilityTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpearheadAbilityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
